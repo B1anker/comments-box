53bc77ef6ef0334698636aef38049be45c7743a1
b1anker <narutowontstop@gmail.com>
2017-02-19 15:33:29 +0800

    Format code

--- app/components/comment.js
+++ app/components/comment.js
@@ -4,13 +4,13 @@
 import React, {Component} from 'react';
 import Remarkable from 'remarkable';
 
-class Comment extends Component{
+class Comment extends Component {
 	rawMakeup() {
 		const md = new Remarkable();
 		let rawMarkup = md.render(this.props.children.toString());
 		return {__html: rawMarkup};
 	}
-	
+
 	render() {
 		return (
 			<div className="comment">
--- app/components/commentBox.js
+++ app/components/commentBox.js
@@ -1,20 +1,19 @@
 /**
  * Created by b1anker on 2017/02/17.
  */
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import CommentList from './commentList';
 import CommentForm from './commentForm';
 
-
-class CommentBox extends Component{
+class CommentBox extends Component {
 	render() {
 		return (
 			<div className="commentBox">
-        <h1>Comments</h1>
-        <CommentList data={this.props.data} />
-        <CommentForm />
-      </div>
+				<h1>Comments</h1>
+				<CommentList data={this.props.data}/>
+				<CommentForm/>
+			</div>
 		)
 	}
 }
