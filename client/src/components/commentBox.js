53a4a0ab952795da85a5dedd121c8cf77b03f0a4
b1anker <narutowontstop@gmail.com>
2017-02-20 22:15:03 +0800

    add xhr of getting data

--- client/src/components/commentBox.js
+++ client/src/components/commentBox.js
@@ -7,11 +7,36 @@ import CommentList from './commentList';
 import CommentForm from './commentForm';
 
 class CommentBox extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: []
+		};
+	}
+
+	componentDidMount() {
+		let xhr = new XMLHttpRequest();
+		xhr.open("get", this.props.url, false);
+		xhr.onreadystatechange = () => {
+			if (xhr.readyState === 4) {
+				if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
+					let data = JSON.parse(xhr.responseText);
+					this.setState({
+						data
+					})
+				} else {
+					console.log(xhr.status);
+				}
+			}
+		}
+		xhr.send(null);
+	}
+
 	render() {
 		return (
 			<div className="commentBox">
-				<h1>Comments1</h1>
-				<CommentList data={this.props.data}/>
+				<h1>Comments</h1>
+				<CommentList data={this.state.data}/>
 				<CommentForm/>
 			</div>
 		)
